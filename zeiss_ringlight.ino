690535e90e8a10d981a6777bd5d90b035969bcac
wes06 <wesley6@gmail.com>
2016-08-21 23:32:00 -0300

    First Commit

---
+++ zeiss_ringlight.ino
@@ -0,0 +1,24 @@
+#include "math.h"
+//double 	pow (double __x, double __y) // x to power of y
+
+void setup(){
+}
+
+int pot = A1;
+int val;
+int led = 0;
+int brilho;
+
+
+void loop(){
+
+  val = analogRead(pot);
+  
+  brilho = round( (pow (2, (val+1)/32 ))/4/*-1*/); // x to power of y
+  brilho = map(brilho, 0, 1023, 128, 255);
+  constrain(brilho, 128, 255);
+
+  analogWrite(led, brilho);
+
+}
+
